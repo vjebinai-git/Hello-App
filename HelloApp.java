acc02b7b0dd34584d1b254148008cd4b10e64274
Jebin V <v.jebinff@gmail.com>
2026-03-26 13:57:24 +0530

    Coded UC7: Hello using String.join()

--- HelloApp.java
+++ HelloApp.java
@@ -4,17 +4,8 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove trailing ", "
-            if (nameBuilder.length() > 0) {
-                String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-                System.out.println("Hello, " + names + "!");
-            }
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
